265d448dab35d7e0806c140993ccd65e684a3b54
Thiago Resmini Slongo <rthiagoslongo@gmail.com>
2026-05-17 15:07:52 -0300

    feat: adiciona perfil candidato e vagas

---
+++ skillmatch.js
@@ -0,0 +1,36 @@
+// Candidatos
+const candidato = {
+        nome: 'Thiago', 
+        area: 'Front-End',
+        habilidades: ['JavaScript', 'React', 'GitHub', 'Lógica de Programação', 'Banco de Dados', 'Kanban'],
+        experienciaMeses: 2
+    },
+
+// Lista de Vagas 
+const vagas = [
+    {
+        id: 1,
+        empresa: 'NewTech',
+        cargo: 'Desenvolvedor Front-End Júnior', 
+        requisitos:['JavaScript', 'GitHub', 'React', 'Lógica de Programação', 'Banco de Dados'],
+        salario: 2700,
+        modalidade: 'Remoto'
+    },
+    {
+        id: 2,
+        empresa: 'LabsCode',
+        cargo: 'Estágio Front-End', 
+        requisitos:['JavaScript', 'GitHub', 'Kanban'],
+        salario: 1500,
+        modalidade: 'Presencial'
+    },
+    {
+        id: 3,
+        empresa: 'NextSolutions',
+        cargo: 'Programador Front-End', 
+        requisitos:['React', 'JavaScript', 'Arrays', 'Objetos', 'POO', 'Funções'],
+        salario: 3500,
+        modalidade: 'Híbrido'
+    },
+]
+
